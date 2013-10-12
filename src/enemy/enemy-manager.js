dbb5dd374f89fd667cb40d8eed869e02cc9e341e
Alex Kalmakov <st00nsa@gmail.com>
2013-10-12 23:59:13 +0400

    delay can be disbled, enabled or removed by delayController
    max zombie spawn from one spwan point == 60

--- src/enemy/enemy-manager.js
+++ src/enemy/enemy-manager.js
@@ -10,6 +10,7 @@ EnemyManager.prototype.constructor = EnemyManager;
 
 EnemyManager.prototype.init = function() {
     this.spawnPoints = [];
+    this.maxSpawnsPerPoint = 50;
 }
 
 EnemyManager.prototype.spawn = function(spawnPoint) {
@@ -21,24 +22,35 @@ EnemyManager.prototype.spawn = function(spawnPoint) {
     else enemyClass = Zombie;
 
     game.createObject2DAt(enemyClass, spawnPoint.x, spawnPoint.y);
+
+    spawnPoint.spawned++;
+    if (spawnPoint.spawned > this.maxSpawnsPerPoint) {
+        spawnPoint.spawnDelayController.remove();
+    }
 }
 
 EnemyManager.prototype.setSpawnPoint = function(x, y, rate) {
     var self = this;
     if (!rate)
         rate = EnemyManager.SPAWN_RATE;
-    this.spawnPoints.push({
+
+    var spawnPoint = {
         x: x,
         y: y,
-        rate: rate
-    });
-    delay(function() {
-        self.spawn({
-            x: x,
-            y: y,
-            rate: rate
-        })
-    }, 1000 * rate, -1);
+        rate: rate,
+        spawned: 0
+    };
+    this.spawnPoints.push(spawnPoint);
+
+    var delayController = delay(
+        function() {
+            self.spawn(spawnPoint);
+        },
+        1000 * rate,
+        -1
+    );
+
+    spawnPoint.spawnDelayController = delayController;
 }
 
 EnemyManager.SPAWN_RATE = 1; //in seconds
