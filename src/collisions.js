cc8f50bfcc630e22b9cc4c1fd5d9916bccb8a361
Alex Kalmakov <st00nsa@gmail.com>
2013-11-17 00:12:53 +0400

    nice map and bullets do not kill player

--- src/collisions.js
+++ src/collisions.js
@@ -10,6 +10,6 @@ Collisions.CATEGORY_GAME_BORDER = 8;
 Collisions.CATEGORY_WALL = 16;
 
 
-Collisions.MASK_PLAYER  = Collisions.CATEGORY_MONSTER | Collisions.CATEGORY_BULLET | Collisions.CATEGORY_GAME_BORDER;
+Collisions.MASK_PLAYER  = Collisions.CATEGORY_MONSTER | Collisions.CATEGORY_GAME_BORDER;
 Collisions.MASK_MONSTER = Collisions.CATEGORY_MONSTER | Collisions.CATEGORY_PLAYER | Collisions.CATEGORY_BULLET;
-Collisions.MASK_BULLET  = Collisions.CATEGORY_PLAYER | Collisions.CATEGORY_MONSTER | Collisions.CATEGORY_BULLET;
+Collisions.MASK_BULLET  = Collisions.CATEGORY_MONSTER | Collisions.CATEGORY_BULLET;
