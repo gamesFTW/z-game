a39f1b4ff68e9918d14c216fb30db400bb981b95
alex-k <alex-k@yandex-team.ru>
2014-02-06 21:45:23 +0400

    + Simple Sector class abstraction

--- src/scenes/sceneMap/mapGenerator.js
+++ src/scenes/sceneMap/mapGenerator.js
@@ -4,167 +4,158 @@ function MapGenerator() {
 
 
 MapGenerator.generateMap = function(width, height, numberOfNodesToRemove) {
-	var Graph = require('data-structures').Graph;
-	var mapGraph = new Graph();
+    var Graph = require('data-structures').Graph;
+    var mapGraph = new Graph();
 
-	MapGenerator._generateGrid(mapGraph, width, height);
+    MapGenerator._generateGrid(mapGraph, width, height);
+    MapGenerator._removeRandomNodes(mapGraph, numberOfNodesToRemove);
 
-	MapGenerator._removeRandomNodes(mapGraph, numberOfNodesToRemove);	
-	
-	return mapGraph;
+    return mapGraph;
 };
 
 
 MapGenerator._removeRandomNodes = function(mapGraph, numberOfremoves) {
-	var allNodes = _.keys(mapGraph._nodes),
-    	i = 0;
+    var allNodes = _.keys(mapGraph._nodes),
+        i = 0;
 
-	while(i != numberOfremoves){
-		var nodeNameForRemove = _.sample(allNodes);
-		if (MapGenerator._removeNodeByName(mapGraph, nodeNameForRemove)) {
-			i++;
-		    _.without(allNodes, nodeNameForRemove);
-		}
-	}
+    while(i != numberOfremoves){
+        var nodeNameForRemove = _.sample(allNodes);
+        if (MapGenerator._removeNodeByName(mapGraph, nodeNameForRemove)) {
+            i++;
+            _.without(allNodes, nodeNameForRemove);
+        }
+    }
 };
 
 
 MapGenerator._removeNodeByName = function(mapGraph, nodeNameToRemove) {
-	var verificationNodeName = null;
+    var verificationNodeName = null;
 
-	for (var nodeName in mapGraph._nodes) {
-		if (nodeName != nodeNameToRemove){
-			verificationNodeName = nodeName;
-			break;
-		}
-	}
+    for (var nodeName in mapGraph._nodes) {
+        if (nodeName != nodeNameToRemove){
+            verificationNodeName = nodeName;
+            break;
+        }
+    }
 
-	var isHasConnection = this._hasConnectionToAll(mapGraph, verificationNodeName, nodeNameToRemove);
+    var isHasConnection = this._hasConnectionToAll(mapGraph, verificationNodeName, nodeNameToRemove);
 
-	if(isHasConnection){
-		mapGraph.removeNode(nodeNameToRemove);
-		return true;
-	}
-	return false;
+    if(isHasConnection){
+        mapGraph.removeNode(nodeNameToRemove);
+        return true;
+    }
+    return false;
 };
 
 
 MapGenerator._hasConnectionToAll = function(mapGraph, startNodeName, nodeNameToIgnore){
-	var visitedNodes = {},
-	    notVisitedNodes = {};
+    var visitedNodes = {},
+        notVisitedNodes = {};
 
-	visitedNodes[nodeNameToIgnore] = true;
-	notVisitedNodes[startNodeName] = true;
+    visitedNodes[nodeNameToIgnore] = true;
+    notVisitedNodes[startNodeName] = true;
 
 
-	mapGraph.getAllBros = function(nodeName) {
+    mapGraph.getAllBros = function(nodeName) {
         return _.keys(this.getNode(nodeName)._outEdges);
-	}
+    }
 
-	while (_.keys(notVisitedNodes).length) {
-		var currentNodeName = _.keys(notVisitedNodes)[0];
+    while (_.keys(notVisitedNodes).length) {
+        var currentNodeName = _.keys(notVisitedNodes)[0];
 
-		var nodes = mapGraph.getAllBros(currentNodeName);
-		for (var i = 0; i < nodes.length; i++) {
-			if (!visitedNodes[nodes[i]]) {
-				notVisitedNodes[nodes[i]] = true;
-			}
-		}
-    
-		visitedNodes[currentNodeName] = true;
-		delete notVisitedNodes[currentNodeName];
-	}
+        var nodes = mapGraph.getAllBros(currentNodeName);
+        for (var i = 0; i < nodes.length; i++) {
+            if (!visitedNodes[nodes[i]]) {
+                notVisitedNodes[nodes[i]] = true;
+            }
+        }
 
-	return _.keys(visitedNodes).length == _.keys(mapGraph._nodes).length;
+        visitedNodes[currentNodeName] = true;
+        delete notVisitedNodes[currentNodeName];
+    }
+
+    return _.keys(visitedNodes).length == _.keys(mapGraph._nodes).length;
 };
 
 
 MapGenerator._generateGrid = function(mapGraph, width, height) {
-	var mapArray = [];
-	id = 0;
+    var mapArray = [];
+    id = 0;
 
-	for (var x = 0; x < width; x++) {
-		mapArray[x] = [];
+    for (var x = 0; x < width; x++) {
+        mapArray[x] = [];
 
-		for (var y = 0; y < height; y++) {
-			var nodeName = id.toString();
-			mapArray[x][y] = nodeName;
+        for (var y = 0; y < height; y++) {
+            var nodeName = id.toString();
+            mapArray[x][y] = nodeName;
 
-			var node = mapGraph.addNode(nodeName);
-			node.x = x * 100;
-			node.y = y * 100;
+            var node = mapGraph.addNode(nodeName);
+            node.positionX = x;
+            node.positionY = y;
 
-			id ++;
-		}		
-	}
+            id ++;
+        }
+    }
 
-	for (x = 0; x < mapArray.length; x++) {
-		for (y = 0; y < mapArray[x].length; y++) {
-			var nodeName = mapArray[x][y];
+    for (x = 0; x < mapArray.length; x++) {
+        for (y = 0; y < mapArray[x].length; y++) {
+            var nodeName = mapArray[x][y];
 
-			if (mapArray[x - 1] !== undefined){
-				if (mapArray[x - 1][y - 1] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x - 1][y - 1]);
-				}
-			}
+            if (mapArray[x - 1] !== undefined){
+                if (mapArray[x - 1][y - 1] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x - 1][y - 1]);
+                }
+            }
 
-			if (mapArray[x] !== undefined){
-				if (mapArray[x][y - 1] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x][y - 1]);
-				}
-			}
+            if (mapArray[x] !== undefined){
+                if (mapArray[x][y - 1] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x][y - 1]);
+                }
+            }
 
-			if (mapArray[x + 1] !== undefined){
-				if (mapArray[x + 1][y - 1] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x + 1][y - 1]);
-				}
-			}
-
-
-			if (mapArray[x - 1] !== undefined){
-				if (mapArray[x - 1][y] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x - 1][y]);
-				}
-			}
-
-			if (mapArray[x] !== undefined){
-				if (mapArray[x][y] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x][y]);
-				}
-			}
-
-			if (mapArray[x + 1] !== undefined){
-				if (mapArray[x + 1][y] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x + 1][y]);
-				}
-			}
+            if (mapArray[x + 1] !== undefined){
+                if (mapArray[x + 1][y - 1] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x + 1][y - 1]);
+                }
+            }
 
 
-			if (mapArray[x - 1] !== undefined){
-				if (mapArray[x - 1][y + 1] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x - 1][y + 1]);
-				}
-			}
+            if (mapArray[x - 1] !== undefined){
+                if (mapArray[x - 1][y] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x - 1][y]);
+                }
+            }
 
-			if (mapArray[x] !== undefined){
-				if (mapArray[x][y + 1] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x][y + 1]);
-				}
-			}
+            if (mapArray[x] !== undefined){
+                if (mapArray[x][y] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x][y]);
+                }
+            }
 
-			if (mapArray[x + 1] !== undefined){
-				if (mapArray[x + 1][y + 1] !== undefined){
-					mapGraph.addEdge(nodeName, mapArray[x + 1][y + 1]);
-				}
-			}
-		}		
-	}
+            if (mapArray[x + 1] !== undefined){
+                if (mapArray[x + 1][y] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x + 1][y]);
+                }
+            }
+
+
+            if (mapArray[x - 1] !== undefined){
+                if (mapArray[x - 1][y + 1] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x - 1][y + 1]);
+                }
+            }
+
+            if (mapArray[x] !== undefined){
+                if (mapArray[x][y + 1] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x][y + 1]);
+                }
+            }
+
+            if (mapArray[x + 1] !== undefined){
+                if (mapArray[x + 1][y + 1] !== undefined){
+                    mapGraph.addEdge(nodeName, mapArray[x + 1][y + 1]);
+                }
+            }
+        }
+    }
 };
-
-function getListFromGraphs(graphs) {
-	var i
-
-	for (i in graphs) {
-
-	}
-}
--- src/scenes/sceneMap/sceneMap.js
+++ src/scenes/sceneMap/sceneMap.js
@@ -13,30 +13,48 @@ SceneMap.prototype.init = function() {
 
     this.pixiStage = new PIXI.Stage(0xEEFFFF, true);
 
-    this.map = new PIXI.DisplayObjectContainer();
-    this.pixiStage.addChild(this.map);
-    this.map.position.x = 50;
-    this.map.position.y = 50;
+    this.displayContainer = new PIXI.DisplayObjectContainer();
+    this.pixiStage.addChild(this.displayContainer);
+    this.displayContainer.position.x = 50;
+    this.displayContainer.position.y = 50;
 
-    this.nodesConteiner = new PIXI.DisplayObjectContainer();
+    this.nodesContainer = new PIXI.DisplayObjectContainer();
     this.edgesContainer = new PIXI.Graphics();
 
-    this.map.addChild(this.edgesContainer);
-    this.map.addChild(this.nodesConteiner);
+    this.displayContainer.addChild(this.edgesContainer);
+    this.displayContainer.addChild(this.nodesContainer);
 
-    this.createGraph();
-    this.drawGraph();
+    this.buildMap();
 
     return this;
 };
 
 SceneMap.prototype.createGraph = function() {
-	this.mapGraph = MapGenerator.generateMap(7, 7, 20);
+    return MapGenerator.generateMap(9, 7, 29);
 };
 
 
+SceneMap.prototype.buildMap = function() {
+    var graph =  this.createGraph(),
+       nodes = _.keys(graph._nodes),
+       self = this;
+
+    this.map = {};
+
+    _.forEach(nodes, function(node) {
+        self.map[node] = (new SimpleSector()).init(node, graph);
+        self.drawNode(self.map[node]);
+    });
+
+    _.forEach(nodes, function(node) {
+        _.keys(self.map[node].graph._outEdges).forEach(function(node2) {
+            node2 = self.map[node2];
+            self.drawEdge(self.map[node], node2);
+        });
+    });
+};
+
 SceneMap.prototype.render = function() {
-    // Рендерим
     game.renderer.render(this.pixiStage);
 };
 
@@ -46,33 +64,54 @@ SceneMap.prototype.loop = function() {
 };
 
 
-SceneMap.prototype.drawGraph = function() {
-	var self = this;
-	this.mapGraph.forEachNode(function(node){
-		self.drawNode(node);
-	});
-};
+//SceneMap.prototype.drawGraph = function() {
+    //var self = this;
+    //_.keys(this.map).forEach(function(node){
+        //self.drawNode(node);
+    //});
+//};
 
 
 SceneMap.prototype.drawNode = function(node) {
-	var brickTexture = PIXI.Texture.fromFrame("img/brick.png");
-
-	var nodeView = new PIXI.Sprite(brickTexture);
-	nodeView.anchor.x = 0.5;
-	nodeView.anchor.y = 0.5;
-	nodeView.position.x = node.x;
-	nodeView.position.y = node.y;
-
-	this.nodesConteiner.addChild(nodeView);
-
-	for (var j in node._outEdges){
-		this.drawEdge(node, this.mapGraph.getNode(j));
-	}
+    this.nodesContainer.addChild(node.view);
 };
 
 
 SceneMap.prototype.drawEdge = function(node, node2) {
-	this.edgesContainer.lineStyle(3, 0x00ff00, 0.5);
-   	this.edgesContainer.moveTo(node.x, node.y);
-   	this.edgesContainer.lineTo(node2.x, node2.y);
+    this.edgesContainer.lineStyle(3, 0x00ff00, 0.5);
+    this.edgesContainer.moveTo(node.view.position.x, node.view.position.y);
+    this.edgesContainer.lineTo(node2.view.position.x, node2.view.position.y);
 };
+
+
+
+
+function SimpleSector() {
+
+}
+
+
+SimpleSector.prototype.constructor = SimpleSector();
+
+
+SimpleSector.prototype.texturePath = "img/brick.png";
+
+
+SimpleSector.prototype.init = function(nodeName, mapGraph) {
+    mapGraph.getNode(nodeName).model = this;
+    this.nodeName = nodeName;
+    this.graph = mapGraph.getNode(nodeName);
+    this.createView();
+    return this;
+};
+
+
+SimpleSector.prototype.createView = function() {
+    var brickTexture = PIXI.Texture.fromFrame(this.texturePath);
+    this.view = new PIXI.Sprite(brickTexture);
+    this.view.anchor.x = 0.5;
+    this.view.anchor.y = 0.5;
+    this.view.position.x = this.graph.positionX * 100;
+    this.view.position.y = this.graph.positionY * 100;
+};
+
