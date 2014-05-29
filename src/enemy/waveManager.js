87947f3583e4bc3961902e76e7905423a26ce8f2
dh <allah.taskbar@gmail.com>
2014-05-29 20:21:27 +0400

    Добавил waveManager.js

---
+++ src/enemy/waveManager.js
@@ -0,0 +1,78 @@
+modules.define(
+    'WaveManager',
+    ['Zombie', 'ZombieDamage', 'ZombieFast'],
+    function(provide, Zombie, ZombieDamage, ZombieFast) {
+
+
+    function WaveManager() {
+    }
+
+    WaveManager.prototype.constructor 	= WaveManager;
+    WaveManager.prototype.waves 		= null;
+
+    WaveManager.prototype.enemiesTypes = [Zombie, ZombieDamage, ZombieFast,
+        ZombieFast, ZombieFast, ZombieFast];
+
+    WaveManager.NUMBER_OF_TYPES_IN_SECTOR = [
+        [5, 2],
+        [20, 3],
+        [50, 4],
+        [20, 5],
+        [5, 6]
+    ];
+
+    WaveManager.NUMBER_OF_TYPES_IN_WAVE = [
+        [5, 1],
+        [20, 2],
+        [40, 3],
+        [25, 4],
+        [5, 5],
+        [5, 6]
+    ];
+
+    WaveManager.DEFAULT_ENEMIES_NUMBER = 1;
+
+    WaveManager.prototype.generateWaves = function(difficulty) {
+        var types = _.sample(
+            this.enemiesTypes,
+            this.randomNumPercent(WaveManager.NUMBER_OF_TYPES_IN_SECTOR)
+        );
+        var waves = [];
+
+        _.forEach([1,2,3], function(i) {
+            var wave = {};
+            var typesInWave = _.sample(
+                types,
+                this.randomNumPercent(WaveManager.NUMBER_OF_TYPES_IN_WAVE)
+            );
+            wave.types = _.map(typesInWave, function(type) {
+                return {
+                    type: type,
+                    number: (WaveManager.DEFAULT_ENEMIES_NUMBER / typesInWave.length) 
+                    	* (difficulty / type.difficulty)
+                };
+            });
+
+            waves.push(wave);
+        }.bind(this));
+
+        this.waves = waves;
+    };
+
+    WaveManager.prototype.randomNumPercent = function(ranges) {
+        var num = null,
+            rnd = _.random(1, 100);
+
+        _.reduce(ranges, function(oldPercent, range) {
+            var percent = range[0] + oldPercent;
+            if (percent >= rnd && num === null) {
+                num = range[1];
+            }
+            return percent;
+        }, 0);
+
+        return num;
+    };
+
+    provide(WaveManager);
+});
